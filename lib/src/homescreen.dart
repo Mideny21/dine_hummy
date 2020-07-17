4605e36959796f48c06128f92ac98700c6d8c607
mideny <mwaikasu21@gmail.com>
2020-07-17 17:52:48 +0300

    making food card

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:dine_hummy/src/widgets/food_category.dart';
 
 import 'widgets/home_top_info.dart';
+import 'package:dine_hummy/src/widgets/bought_foods.dart';
+
+import 'package:dine_hummy/src/widgets/search_field.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -18,6 +21,35 @@ class _HomeScreenState extends State<HomeScreen> {
         children: <Widget>[
           HomeTopInfo(),
           FoodCategory(),
+          SizedBox(
+            height: 20.0,
+          ),
+          SearchField(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Frequently Bought Foods",
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "View All",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.greenAccent),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            child: BoughtFoods(),
+          ),
         ],
       ),
     );
